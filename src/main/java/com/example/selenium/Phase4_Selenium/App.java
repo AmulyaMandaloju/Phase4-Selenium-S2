9a858e1913718857309bba6f99d5287a539936bb
Admin <admin@gmail.com>
2024-02-10 20:45:19 +0530

    Addedd loginpage , listners and extent report

---
+++ src/main/java/com/example/selenium/Phase4_Selenium/App.java
@@ -0,0 +1,13 @@
+package com.example.selenium.Phase4_Selenium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
